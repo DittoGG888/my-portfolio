ecc65abe72340edb4f7aa292e44f0dd1233ae031
ThabangXist <thabang@existence-klcorp.com>
2025-02-08 17:10:01 +0200

    First Commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-center text-green-600 dark:invert-0">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
